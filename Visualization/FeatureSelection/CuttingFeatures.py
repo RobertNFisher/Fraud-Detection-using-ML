0ac16522712e53c1c5f5cd52e4a946a2e8484d6d
TFerd <54553544+TFerd@users.noreply.github.com>
2019-11-03 19:52:24 -0500

    Add files via upload
    Messing around with sampling the main file. Removed features based on correlation and created a new csv file called 'Removed_Features.csv'

---
+++ Visualization/FeatureSelection/CuttingFeatures.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+f = open("FeatureCut.txt", "r")
+f_array = []
+rd = []
+
+data = pd.read_csv("Training_Sample.csv", sep=",", index_col=0)
+
+
+for line in f:
+    f_array.append(line)
+
+for i in f_array:
+    if i not in rd:
+        rd.append(i)
+
+
+print(len(f_array))
+print(len(rd))
+print(''.join(rd))
+for i in rd:
+    temp = i.replace("\n","")
+    data.drop(temp, axis = 1, inplace = True)
+
+
+
+
+data.to_csv(r'Removed_Features.csv')
+
+
+f.close()
